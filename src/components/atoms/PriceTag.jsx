d73d62320b826d626ce0fe6cbbebdc34f978dba2
Flesky <juandmr722@gmail.com>
2026-04-17 17:54:09 -0500

    feat(atoms): agregado el priceTag con función de formato en dolares

---
+++ src/components/atoms/PriceTag.jsx
@@ -0,0 +1,8 @@
+export default function PriceTag({ price }) {
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+
+  return <p>{formattedPrice}</p>;
+}
