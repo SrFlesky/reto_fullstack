f1bcbcb8f56f9096938e03a6968741ebb94a067d
Flesky <juandmr722@gmail.com>
2026-04-20 14:29:26 -0500

    feat(Contact): agregar estructura con info y formulario sin logica

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,29 @@
+import { Button } from "../../components/atoms/Button";
+
 export default function Contact() {
+    const notWorking = () => alert("En desarrollo...");
   return (
     <div>
-      <h1>Contacto</h1>
+      {/* Lado izquierdo */}
+      <div>
+        <span>Información</span>
+        <h1>Contáctanos</h1>
+        <p>Proyecto final del curso FullStack — tienda de moda y accesorios.</p>
+
+        <div>
+          <h2>Colombia</h2>
+          <p>Pasto, Nariño</p>
+          <p>+57 000 000 0000</p>
+        </div>
+      </div>
+
+      {/* Lado derecho*/}
+      <div>
+        <input placeholder="Nombre" />
+        <input placeholder="Email" />
+        <textarea placeholder="Mensaje" />
+        <Button variant={"primary"} onClick={notWorking}>Enviar mensaje</Button>
+      </div>
     </div>
   );
 }
