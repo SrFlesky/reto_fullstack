bf7130f62c4c042cfcbd9dc1792ab340b671ad97
Flesky <juandmr722@gmail.com>
2026-04-22 22:46:45 -0500

    style(Shop): agregar margen horizontal al mosaico de productos

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -42,7 +42,7 @@ function Home() {
         </label>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 m-8">
         {filteredProducts.map((product) => (
           <ProductCard
             key={product.id}
