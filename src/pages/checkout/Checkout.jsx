fa261f296bee3461420056e6d5dc1311ef02ec6b
Flesky <juandmr722@gmail.com>
2026-04-23 16:27:28 -0500

    feat(Checkout): agregados los formularios de info y pago

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,7 +1,37 @@
 export default function Checkout() {
   return (
     <div>
-      <h1>Checkout</h1>
+
+      {/* Formulario */}
+      <div>
+        <h1>Checkout</h1>
+
+        <div>
+          <h2>Información de envío</h2>
+          <input placeholder="Nombre" />
+          <input placeholder="Apellido" />
+          <input placeholder="Email" />
+          <input placeholder="Dirección" />
+          <input placeholder="Ciudad" />
+          <input placeholder="País" />
+        </div>
+
+        <div>
+          <h2>Información de pago</h2>
+          <input placeholder="Número de tarjeta" />
+          <input placeholder="Fecha de vencimiento" />
+          <input placeholder="CVV" />
+        </div>
+
+        <button>Pagar</button>
+      </div>
+
+      {/* Lado derecho — resumen del pedido */}
+      <div>
+        <h2>Resumen del pedido</h2>
+        <div>{/* items del carrito */}</div>
+        <p>Total</p>
+      </div>
     </div>
-  )
+  );
 }
