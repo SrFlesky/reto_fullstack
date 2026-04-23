1032ab556aed013fcaa3fb4a65ade445ce84fdd8
Flesky <juandmr722@gmail.com>
2026-04-22 22:16:15 -0500

    feat(Shop): agregar una barra de búsqueda para filtrar elementos por nombre

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,7 +1,37 @@
-export default function Shop() {
+import { ProductCard } from "../../components/molecules/ProductCard";
+import { mockProducts } from "../../mockdata/products";
+import useCartStore from "../../store/cartStore";
+import { useState } from "react";
+
+const calling = () => alert("Funcionando");
+
+function Home() {
+  const { addItem } = useCartStore();
+  const [search, setSearch] = useState("");
+  const filteredProducts = mockProducts.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div>
-      <h1>Tienda</h1>
+    <div className="">
+      <input
+        placeholder="Buscar productos..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className=""
+      />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {filteredProducts.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            onAddToCart={() => addItem(product)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
+
+export default Home;
