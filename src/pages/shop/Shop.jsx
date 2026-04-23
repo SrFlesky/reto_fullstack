abefa76135d674bcff956504ff738c534f6c99d2
Flesky <juandmr722@gmail.com>
2026-04-22 22:25:13 -0500

    style(Shop): estilizar la navbar y diseño responsive

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -2,6 +2,7 @@ import { ProductCard } from "../../components/molecules/ProductCard";
 import { mockProducts } from "../../mockdata/products";
 import useCartStore from "../../store/cartStore";
 import { useState } from "react";
+import { Search } from "lucide-react";
 
 const calling = () => alert("Funcionando");
 
@@ -13,13 +14,22 @@ function Home() {
   );
 
   return (
-    <div className="">
-      <input
-        placeholder="Buscar productos..."
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        className=""
-      />
+    <div className="p-0">
+      <div className="flex justify-center px-8 mb-8">
+        <label
+          htmlFor="search"
+          className="relative flex items-center cursor-text w-full max-w-md bg-white"
+        >
+          <Search size={16} className="absolute left-3 text-gray-400" />
+          <input
+            id="search"
+            placeholder="Buscar producto"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="w-full pl-10 pr-4 py-3 text-sm outline-none border border-gray-200 focus:border-black transition-colors duration-200 bg-white"
+          />
+        </label>
+      </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {filteredProducts.map((product) => (
