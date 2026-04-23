957c3360d0856d83fb18b1cda875791c449225bc
Flesky <juandmr722@gmail.com>
2026-04-22 22:04:16 -0500

    style(Contact): modificar disposicion del flex

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../../components/atoms/Button";
 
 export default function Contact() {
+  const onClick = ()=> {alert("En desarrollo...")}
   return (
     <div>
       <div className="bg-white grid grid-cols-1 md:grid-cols-2 gap-16 px-8 py-20 max-w-6xl mx-auto">
@@ -17,10 +18,17 @@ export default function Contact() {
           </div>
 
           <div className="flex flex-col gap-2">
-            <h2 className="text-base font-medium">Colombia</h2>
-            <p className="text-sm text-gray-500">Pasto, Nariño</p>
+            <h2 className="text-base font-medium">Antioquia</h2>
+            <p className="text-sm text-gray-500">Medellín</p>
             <p className="text-sm text-gray-500">+57 000 000 0000</p>
           </div>
+
+          <div className="flex flex-col gap-2">
+            <h2 className="text-base font-medium">Nariño</h2>
+            <p className="text-sm text-gray-500">San Juan de Pasto</p>
+            <p className="text-sm text-gray-500">+57 000 000 0000</p>
+          </div>
+
         </div>
 
         {/* Lado derecho */}
@@ -40,7 +48,7 @@ export default function Contact() {
             rows={6}
             className="border border-gray-200 px-4 py-3 text-sm outline-none focus:border-black transition-colors duration-200 resize-none"
           />
-          <button className="bg-black text-white px-8 py-4 text-xs uppercase tracking-widest hover:bg-[#E8540A] transition-colors duration-200 cursor-pointer self-start">
+          <button onClick={onClick} className="bg-black text-white px-8 py-4 text-xs uppercase tracking-widest hover:bg-[#E8540A] transition-colors duration-200 cursor-pointer self-start">
             Enviar mensaje
           </button>
         </div>
